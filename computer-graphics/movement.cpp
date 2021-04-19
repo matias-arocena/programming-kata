0abeabfb485a091b72ab5f4db52bfb36c6556f40
Matias Rodriguez Arocena <matias.ra@globant.com>
2021-04-19 10:41:29 -0300

    Refactor

--- computer-graphics/movement.cpp
+++ computer-graphics/movement.cpp
@@ -5,6 +5,103 @@
 #include <cmath>
 #include <chrono>
 
+struct GameState {
+    GLdouble zoom , cameraX, cameraY, degrees;
+    bool fullscreen, running;
+    SDL_Window* window;
+};
+
+
+void ProcessInput(struct GameState* gs) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    if (event.type == SDL_KEYDOWN) {
+        switch (event.key.keysym.sym) {
+        case SDLK_ESCAPE:
+            gs->running = false;
+            break;
+        case SDLK_KP_PLUS:
+            gs->zoom -= 0.4;
+            break;
+        case SDLK_KP_MINUS:
+            gs->zoom += 0.4;
+            break;
+        case SDLK_UP:
+            gs->cameraY += 0.01;
+            break;
+        case SDLK_DOWN:
+            gs->cameraY -= 0.01;
+            break;
+        case SDLK_LEFT:
+            gs->cameraX -= 0.01;
+            break;
+        case SDLK_RIGHT:
+            gs->cameraX += 0.01;
+            break;
+        case SDLK_c:
+            gs->cameraX = 0;
+            gs->cameraY = 0;
+            gs->zoom = 15;
+            break;
+        case SDLK_F11:
+            if (gs->fullscreen) {
+                SDL_SetWindowFullscreen(gs->window, SDL_FALSE);
+            }
+            else {
+                SDL_SetWindowFullscreen(gs->window, SDL_WINDOW_FULLSCREEN);
+            }
+            gs->fullscreen = !gs->fullscreen;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Render(double deltaTime, struct GameState* gs) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0, 0, gs->zoom, gs->cameraX, gs->cameraY, 0, 0, 1, 0);
+
+    gs->degrees = fmod(gs->degrees + 0.072 * deltaTime, 360);
+    glRotated(gs->degrees, 0, 0, -6);
+
+    glTranslatef(-1.5f, 0.0f, -5.9f);
+
+    glPushMatrix();
+    glRotated(gs->degrees, 0, 1, 0);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1, 0, 0);
+    glVertex3d(0, 1, 0);
+    glColor3f(0, 1, 0);
+    glVertex3d(-1, -1, 0);
+    glColor3f(0, 0, 1);
+    glVertex3d(1, -1, 0);
+    glEnd();
+    glPopMatrix();
+
+    glTranslatef(3, 0, 0);
+
+    glPushMatrix();
+    glRotated(-gs->degrees, 0, 1, 0);
+    glBegin(GL_POLYGON);
+    glColor3f(0.4f, 0.4f, 1.f);
+    glVertex3d(-1, 1, 0);
+    glVertex3d(1, 1, 0);
+    glVertex3d(1, -1, 0);
+    glVertex3d(-1, -1, 0);
+    glEnd();
+    glPopMatrix();
+}
+
+void GameLoop(double deltaTime, GameState* gs) {
+    ProcessInput(gs);
+
+    Render(deltaTime, gs);
+}
+
+
 int main(int argv, char** args) {
     SDL_Window* window = SDL_CreateWindow("colorPolygon",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
@@ -24,110 +121,36 @@ int main(int argv, char** args) {
             << std::endl;
     }
 
+    struct GameState* gs = new GameState;
+    gs->zoom = 15;
+    gs->cameraX = 0;
+    gs->cameraY = 0;
+    gs->degrees = 0;
+    gs->fullscreen = false;
+    gs->running = true;
+    gs->window = window;
+
     glMatrixMode(GL_PROJECTION);
     glClearColor(0, 0, 0, 1);
     gluPerspective(45, 640 / 480, 0.1, 100);
     glMatrixMode(GL_MODELVIEW);
 
-    GLdouble zoom = 15;
-    GLdouble cameraX = 0;
-    GLdouble cameraY = 0;
-    GLfloat degrees = 0;
-
-    bool fullscreen = false;
-
-    bool running = true;
+    
     auto newTime = std::chrono::system_clock::now();
-    while (running) {
+    while (gs->running) {
         auto oldTime = newTime;
         newTime = std::chrono::system_clock::now();
         double deltaTime = std::chrono::duration<double, std::milli>(newTime - oldTime).count();
-
-        SDL_Event event;
-        SDL_PollEvent(&event);
-
-        if (event.type == SDL_KEYDOWN) {
-            switch (event.key.keysym.sym) {
-            case SDLK_ESCAPE:
-                running = false;
-                break;
-            case SDLK_KP_PLUS:
-                zoom -= 0.4;
-                break;
-            case SDLK_KP_MINUS:
-                zoom += 0.4;
-                break;
-            case SDLK_UP:
-                cameraY += 0.01;
-                break;
-            case SDLK_DOWN:
-                cameraY -= 0.01;
-                break;
-            case SDLK_LEFT:
-                cameraX -= 0.01;
-                break;
-            case SDLK_RIGHT:
-                cameraX += 0.01;
-                break;
-            case SDLK_c:
-                cameraX = 0;
-                cameraY = 0;
-                zoom = 15;
-                break;
-            case SDLK_F11:
-                if (fullscreen) {
-                    SDL_SetWindowFullscreen(window, SDL_FALSE);
-                }
-                else {
-                    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-                }
-                fullscreen = !fullscreen;
-                break;
-            default:
-                break;
-            }
-        }
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glLoadIdentity();
-        gluLookAt(0, 0, zoom, cameraX, cameraY, 0, 0, 1, 0);
-
-        degrees = fmod(degrees + 0.072 * deltaTime, 360);
-        glRotatef(degrees, 0, 0, -6);
-
-        glTranslatef(-1.5f, 0.0f, -5.9f);
-
-        glPushMatrix();
-        glRotatef(degrees, 0, 1, 0);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1, 0, 0);
-        glVertex3d(0, 1, 0);
-        glColor3f(0, 1, 0);
-        glVertex3d(-1, -1, 0);
-        glColor3f(0, 0, 1);
-        glVertex3d(1, -1, 0);
-        glEnd();
-        glPopMatrix();
-
-        glTranslatef(3, 0, 0);
-
-        glPushMatrix();
-        glRotatef(-degrees, 0, 1, 0);
-        glBegin(GL_POLYGON);
-        glColor3f(0.4f, 0.4f, 1.f);
-        glVertex3d(-1, 1, 0);
-        glVertex3d(1, 1, 0);
-        glVertex3d(1, -1, 0);
-        glVertex3d(-1, -1, 0);
-        glEnd();
-        glPopMatrix();
-
+        GameLoop(deltaTime, gs);
+        
         SDL_GL_SwapWindow(window);
     }
+
+    gs->window = nullptr;
+    delete gs;
 
     SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
-
